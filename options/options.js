229d56e6cf9ae1f210381453fb21d5db5b3e00c0
Crowdbotics <engineering@crowdbotics.com>
2023-12-19 10:14:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "checkconnectorapi--131853",
+  url: "https://checkconnectorapi--131853.botics.co",
+  api: "https://checkconnectorapi--131853.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
